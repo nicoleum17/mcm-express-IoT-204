5a0580b848fd467e9d8deff88f949c42a601f53c
Nicole Uriostegui <a01711853@tec.mx>
2024-10-08 17:54:02 -0600

    feat: more settings

---
+++ src/index.ts
@@ -0,0 +1,10 @@
+import { config } from "dotenv";
+import express from "express";
+//Para poder acceder a las variables del ambiente (.env)
+config();
+const app = express();
+console.log("Hola mundo");
+console.log("Holis");
+app.listen(process.env.SERVER_PORT, function () {
+  console.log("Escuchando puerto " + process.env.SERVER_PORT);
+});
