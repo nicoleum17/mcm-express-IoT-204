b1a96cc5b7018f3afee07e1474b27b1dede2df0c
Nicole Uriostegui <a01711853@tec.mx>
2024-10-08 17:05:29 -0600

    feat:enviroment settings

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,14 +1,13 @@
 import globals from "globals";
 import pluginJs from "@eslint/js";
 import tseslint from "typescript-eslint";
-import eslintPluginPrettierRecommended from "eslint.plugin-prettier/recommended";
-
+import eslintPluginPrettierRecommended from "eslint-plugin-prettier/recommended";
 
 export default [
-  { ignores: ["dist/"]},
-  { files: ["ssrc/**/*.{js,ts,jsx,tsx}"]},
-  {files: ["**/*.js"], languageOptions: {sourceType: "commonjs"}},
-  {languageOptions: { globals: globals.node }},
+  { ignores: ["dist/"] },
+  { files: ["ssrc/**/*.{js,ts,jsx,tsx}"] },
+  { files: ["**/*.js"], languageOptions: { sourceType: "commonjs" } },
+  { languageOptions: { globals: globals.node } },
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
   eslintPluginPrettierRecommended,
