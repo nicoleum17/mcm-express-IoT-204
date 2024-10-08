1960cae8b58c5c5c0a9e10aadc472609651b9796
Nicole Uriostegui <a01711853@tec.mx>
2024-10-08 16:03:55 -0600

    feat:commit

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,7 @@
 import globals from "globals";
 import pluginJs from "@eslint/js";
 import tseslint from "typescript-eslint";
+import eslintPluginPrettierRecommended from "eslint.plugin-prettier/recommended";
 
 
 export default [
@@ -10,4 +11,5 @@ export default [
   {languageOptions: { globals: globals.node }},
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
+  eslintPluginPrettierRecommended,
 ];
