41201450b6d9c9669162a25be3230745e5ecaf66
Nicole Uriostegui <a01711853@tec.mx>
2024-10-18 09:19:18 -0600

    feat: detalles finales

--- src/controllers/student.ts
+++ src/controllers/student.ts
@@ -1,6 +1,6 @@
 import { Request, Response } from "express";
 
-import { findAll } from "../services/student";
+import { deleteById, findAll, insert, update } from "../services/student";
 import { Student } from "../interfaces/student";
 
 // Obtener todos los alumnos
@@ -10,5 +10,36 @@ export const getStudents = async (req: Request, res: Response) => {
     res.status(200).json(students);
   } catch (error) {
     res.status(400).json({ message: "Error al obtener alumnos", error });
+  }
+};
+
+export const createStudent = async (req: Request, res: Response) => {
+  try {
+    const student: Student = req.body;
+    await insert(student);
+    res.status(201).json({ message: "Alumno creado exitosamente" });
+  } catch (error) {
+    res.status(400).json({ message: "Error al crear alumno", error });
+  }
+};
+
+export const updateStudent = async (req: Request, res: Response) => {
+  try {
+    const id = Number.parseInt(req.params.id);
+    const student: Student = req.body;
+    await update(id, student);
+    res.status(201).json({ message: "Alumno actualizado exitosamente" });
+  } catch (error) {
+    res.status(400).json({ message: "Error al actualizar alumno", error });
+  }
+};
+
+export const deleteStudent = async (req: Request, res: Response) => {
+  try {
+    const id = Number.parseInt(req.params.id);
+    await deleteById(id);
+    res.status(201).json({ message: "Alumno actualizado exitosamente" });
+  } catch (error) {
+    res.status(400).json({ message: "Error al actualizar el alumno", error });
   }
 };
--- src/routes/student.ts
+++ src/routes/student.ts
@@ -1,10 +1,18 @@
 // routes/usuarios.ts
 import { Router } from "express";
-import { getStudents } from "../controllers/student";
+import {
+  createStudent,
+  deleteStudent,
+  getStudents,
+  updateStudent,
+} from "../controllers/student";
 
 const router = Router();
 
 // Regresa todos los alumnos en la base de datos
 router.get("/", getStudents);
+router.post("/", createStudent);
+router.put("/:id", updateStudent);
+router.delete("/:id", deleteStudent);
 
 export default router;
